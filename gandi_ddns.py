7ab6abfa5cd363210049e3df6f41f5e98b1959db
Rich McAllister <rfm@pensfa.org>
2023-04-05 18:59:24 -0700

    Handle protocol = none

--- gandi_ddns.py
+++ gandi_ddns.py
@@ -90,6 +90,8 @@ def apply_config_defaults(sec):
         m.put(msg.ERROR,'Invalid protocols value \"%s\", fix config.txt' % (
             sec['protocols']))
         sys.exit(2)
+    if sec['protocols'] = 'none':
+        sec['protocols'] = ''
     if not sec.get('ttl'):
         sec['ttl'] = '900'
     if not sec.get('api'):
